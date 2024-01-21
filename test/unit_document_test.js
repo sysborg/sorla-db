5ae6e1462cbeac04c1d813339ff3337d9f5a02db
Anderson <andmarruda@gmail.com>
2024-01-20 23:04:06 -0300

    preparing test for document capabilities

---
+++ test/unit_document_test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+const faker = require('faker');
+const documents = require('../src/documents/document.js');
+
+describe('testing document capabilities', () => {
+
+});
