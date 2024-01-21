9bb0de814035687e5326f5c0eca19ca2278a1b17
Anderson <andmarruda@gmail.com>
2024-01-20 23:37:20 -0300

    preparing some documents test to understand some behaviours and improving search capabilities

--- test/unit_document_test.js
+++ test/unit_document_test.js
@@ -4,5 +4,36 @@ const faker = require('faker');
 const documents = require('../src/documents/document.js');
 
 describe('testing document capabilities', () => {
+    it('checking capabilities of getting a field inside a object', () => {
+        const doc = new documents({
+            name: 'Anderson',
+            test: {
+                inscription: 123
+            }
+        });
+        
+        expect(doc.name).to.equal('Anderson');
+    });
 
+    it('checking capabilities of getting a field inside a object', () => {
+        const doc = new documents({
+            name: 'Anderson',
+            test: {
+                inscription: 123
+            }
+        });
+        
+        expect(doc.test.inscription).to.equal(123);
+    });
+
+    it('checking capabilities of getting a field inside a object', () => {
+        const doc = new documents({
+            name: 'Anderson',
+            test: {
+                inscription: 123
+            }
+        });
+        
+        expect(doc['test.inscription']).to.equal(123);
+    });
 });
